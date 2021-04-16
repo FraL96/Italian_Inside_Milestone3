7c12f3be218f34204db1ee594c999f1bda8e0f81
FraL96 <francesca_lupu@live.it>
2021-04-16 16:58:16 +0000

    Add profile page

--- app.py
+++ app.py
@@ -42,6 +42,7 @@ def register():
 
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
 
     return render_template("register.html")
 
@@ -58,6 +59,8 @@ def login():
                 session["user"] = request.form.get("username").lower()
                 flash("Welcome back, {}. We missed you!".format(
                     request.form.get("username")))
+                return redirect(url_for("profile", username=session["user"]))
+
             else:
                 flash("Ops! Username and/or Password are incorrect")
                 return redirect(url_for("login"))
@@ -67,6 +70,13 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":
